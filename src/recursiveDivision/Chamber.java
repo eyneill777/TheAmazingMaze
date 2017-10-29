bc44399a81c0556a0c3649d05d65ff83181d3d2a
eronneill <eronneill@192.168.0.8>
2017-10-29 16:46:21 -0400

    Added back in the main class to fix conflicts

---
+++ src/recursiveDivision/Chamber.java
@@ -0,0 +1,13 @@
+package recursiveDivision;
+
+import main.Cell;
+
+public class Chamber 
+{
+	Cell[] cells;
+	
+	public Chamber(Cell[] cells)
+	{
+		this.cells = cells;
+	}
+}
---
+++ src/recursiveDivision/RecursiveDivision.java
@@ -0,0 +1,20 @@
+package recursiveDivision;
+
+import main.Cell;
+import main.Maze;
+import main.MazeGenerator;
+
+public class RecursiveDivision extends MazeGenerator
+{
+	public RecursiveDivision(Maze maze) 
+	{
+		super(maze);
+	}
+
+	@Override
+	public void generateMaze(Cell[] cells) 
+	{
+		int x = 0, y = 0;
+		
+	}
+}
