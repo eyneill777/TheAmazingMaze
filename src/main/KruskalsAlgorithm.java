5ee94e0314996e5040986db9199378090bde3c84
tln86 <tln86@192.168.0.6>
2017-11-06 11:59:28 -0500

    Added some comments to kruskal's and cleaned it up

--- src/main/KruskalsAlgorithm.java
+++ src/main/KruskalsAlgorithm.java
@@ -1,17 +1,12 @@
 package main;
 
-import java.awt.Dimension;
-import java.awt.image.BufferedImage;
-import java.lang.reflect.Array;
-import java.security.KeyStore.Entry;
+/**
+ * @author tln86
+ */
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 
@@ -48,13 +43,11 @@ public class KruskalsAlgorithm extends MazeGenerator {
 
 		}
 
-
 		// Removing all the outside walls
 		for (int i = walls.size() - 1; i >= 0; i--) {
 			if (walls.get(i).isOutsideWall())
 				walls.remove(i);
 		}
-		System.out.println(walls.size()+"\t"+walls);
 		// Will shuffle the walls
 		for (int i = 0; i < walls.size(); i++) {
 			int ind = rand.nextInt(walls.size());
@@ -62,16 +55,19 @@ public class KruskalsAlgorithm extends MazeGenerator {
 			Collections.swap((List<Wall>) walls, ind, ind2);
 		}
 
-		// Playing with how I am going to delete the walls. The following does not
-		// reflect Kruskal's at all.
-		// How do i give each cell its own set?
+		// 1. Pick a wall from the list of walls.
+		// 2. If both cells have not been visited, mark them as visited and join their sets. Knock the wall down.
+		// 3. If one has been visited and the other has not, mark it visited and join it with the larger set. Knock the wall down.
+		// 4. If both have been visited and are in different set, knock the wall down and join their sets.
+		// 5. If both are visited and in the same sets, keep the wall and toss it out of the list.
+		// 6. once all walls have been used, stop.
 
 		while (!walls.isEmpty()) {
 			Cell tempCell = walls.get(0).getCell1();
 			Cell tempCell2 = walls.get(0).getCell2();
 			Direction tempD = walls.get(0).getRelativeCellPosition();
 			if (tempCell.getLabel() == tempCell2.getLabel()) {
-				// System.out.println("0 " +tempCell + " " + tempCell2);
+
 				walls.remove(0);
 			} else if (!visited.contains(tempCell) && !visited.contains(tempCell2)) { // Cells are in their own sets and
 				// haven't been visited
@@ -81,38 +77,32 @@ public class KruskalsAlgorithm extends MazeGenerator {
 				visited.add(tempCell2);
 				tempCell.setLabel(tempCell2.getLabel()); // Give cell 2 the same label as cell 1. Put them in the same
 															// set.
-				// System.out.println("1 " +tempCell + " " + tempCell2);
 			} else if (!visited.contains(tempCell)) { // Cell 1 is in it's own set. Cell2 is in a larger set.
 				visited.add(tempCell);
 				tempCell.removeWall(tempD);
 				walls.remove(0);
 				tempCell.setLabel(tempCell2.getLabel());
-				// System.out.println("2 " +tempCell + " " + tempCell2);
-
 			} else if (!visited.contains(tempCell2)) { // Cell 2 is in it's own set. Cell2 is in a larger set.
 				visited.add(tempCell2);
 				tempCell.removeWall(tempD);
 				walls.remove(0);
 				tempCell2.setLabel(tempCell.getLabel());
-				// System.out.println("3 " +tempCell + " " + tempCell2);
 			} else if (visited.contains(tempCell) && visited.contains(tempCell2)) { // cell 2 and cell 1 have been
 				walls.remove(0);
 				if (tempCell.getLabel() != tempCell2.getLabel()) { // visited but are in different sets.
-					// System.out.println(tempCell +" "+ tempCell2);
 					tempCell.removeWall(tempD);
 				}
 				int label = tempCell.getLabel();
-				
+
 				for (Cell c : visited) {
 					if (c.getLabel() == label && !c.equals(tempCell)) {
 						c.setLabel(tempCell2.getLabel());
 					}
 				}
 				tempCell.setLabel(tempCell2.getLabel());
-				
+
 			}
 		}
-		System.out.println("visited " + visited);
 	}
 
 }
